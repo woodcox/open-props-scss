3c2871018dd6ad3ac3a5bf77fc914c12b5a016c7
Rustie <64870518+woodcox@users.noreply.github.com>
2023-05-23 15:35:20 +0100

    Update generateScss.mjs

--- generateScss.mjs
+++ generateScss.mjs
@@ -76,7 +76,7 @@ const generateSCSSModule = async (moduleName, importObj) => {
     Object.entries(importObj).forEach(([key, value]) => {
       if (key.includes('@media:dark')) {
         // Create @media dark mode @keyframes
-        key = key.replace(/--|-@|animation-/g, 'dark-');
+        key = key.replace(/--|-@|animation-|@media:/g, 'dark-');
         mediaStr += `@mixin ${key}{@media #{_mq.$OSdark} { ${value} }}\n`;
       } else if (value.includes('@keyframes')) {
         key = key.replace(/--|-@|animation-/g, '');
