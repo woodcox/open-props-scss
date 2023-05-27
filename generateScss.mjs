5fbddbf1f9e3c218f2c3c02660f72e238efd1204
Rustie <64870518+woodcox@users.noreply.github.com>
2023-05-27 05:18:44 +0100

    Update generateScss.mjs

--- generateScss.mjs
+++ generateScss.mjs
@@ -129,7 +129,9 @@ const generateSCSSModule = async (moduleName, importObj) => {
           // Create CSS: Sass key-value pairs from a map
           cssSassVarMap += uniqueCssVarStr
             .map(varName => `--${varName}: #{$${varName}}|`)
-            .split('|');
+            //.split('|');
+            .join('')
+            .replace(/\|$/, '');
         }
       }
     });
