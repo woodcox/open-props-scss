cbe8473bda4b6d3172b096a9817161d772de0837
Rustie <64870518+woodcox@users.noreply.github.com>
2023-05-30 15:27:22 +0100

    Added animations and shadows test modules

--- generateScss.mjs
+++ generateScss.mjs
@@ -1,3 +1,5 @@
+// very helpful - https://medium.com/beyn-technology/using-css-variables-in-scss-functions-9521be4de4e3
+
 import Sizes from 'open-props/src/sizes';
 import Colors from 'open-props/src/colors';
 import ColorsHsl from 'open-props/src/colors-hsl';
@@ -46,6 +48,10 @@ const writeSCSSModule = async (moduleName, content) => {
 
 const generateSCSSModule = async (moduleName, importObj) => {
   let generatedScss = '';
+  
+//=========================
+// Aspects
+//=========================
 
   if (moduleName.toLowerCase() === 'aspects') {
     generatedScss = '@use "sass:list";\n';
@@ -57,16 +63,88 @@ const generateSCSSModule = async (moduleName, importObj) => {
       }
       generatedScss += `${key}: ${value};\n`;
     });
+    
+  //=========================
+  // Media
+  //=========================
   } else if (moduleName.toLowerCase() === 'media') {
     Object.keys(importObj).forEach((queryName) => {
       const processedQuery = customMediaHelper.process(queryName);
       queryName = queryName.replace('--', '$');
       generatedScss += `${queryName}: '${processedQuery}';\n`;
     });
+    
+   //=========================
+  // Animations
+  //=========================  
+  } else if (moduleName.toLowerCase() === 'animations') {
+    generatedScss = '@use "easings" as _e;\n@use "media" as _mq;\n';
+    let animationsStr = '';
+    let keyframesStr = '';
+    let mediaStr = '';
+    
+    Object.entries(importObj).forEach(([key, value]) => {
+      if (key.includes('@media:dark')) {
+        key = key.replace(/--|@media:|animation-/g, '');
+        mediaStr += `@mixin ${key}{@media #{_mq.$OSdark} { ${value} }}\n`; // Create sass mixin for @media dark mode
+      } else if (value.includes('@keyframes')) {
+        key = key.replace(/--|-@|animation-/g, '');
+        keyframesStr += `@mixin ${key}{${value}}\n`; // create @keyframes sass mixins
+      } else {
+        key = key.replace('--', '$');
+        const sassVar = value.replace(/var\(--(.*?)\)/g, 'var(#{_e.$$$1})'); // Replace var(--cssvar) with e.$cssvar when they occurs in a value
+        animationsStr += `${key}: ${sassVar};\n`;
+      }
+    });
+    generatedScss += `${animationsStr}${keyframesStr}\n${mediaStr}`;
+  
+  //=========================
+  // Shadows
+  //=========================
+  // Shadows uses hsl colors in '--shadow-color: 220 3% 15%;'. However when this is converted to a sass variable sass will throw an error because the sass variable is not split into $hue, $saturation and $lightness.
+  // Therefore must use dynamic css variables for shadows. This also affects the dark mode.
+  } else if (moduleName.toLowerCase() === 'shadows') {
+    generatedScss = '@use "media" as _mq;\n';
+    let darkMediaStr = '';
+    let cssSassVarStr = '';
+    
+    Object.entries(importObj).forEach(([key, value]) => {
+      if (key.includes('-@media:dark')) {
+        const mediaKey = key.replace(/--([^@]*)-@media:dark/, '--$1');
+        darkMediaStr += `${mediaKey}: ${value};`;
+      } else {
+        key = key.replace('--', '$');
+        generatedScss += `${key}: ${value};\n`;
+        
+        // Extract CSS variable names
+        const cssVarNames = value.match(/var\(--(.*?)\)/g)?.map((match) => match.match(/var\(--(.*?)\)/)[1]);
+        
+        if (cssVarNames && cssVarNames.length > 0) {
+          // Remove duplicates and empty string from cssVarNames 
+          const uniqueCssVarStr = [...new Set(cssVarNames)];
+          
+          // Create CSS: Sass key-value pairs and split using || as a delimiter
+          cssSassVarStr += uniqueCssVarStr
+            .map(varName => `--${varName}: #{$${varName}}||`)
+            .join('')
+            .split('||');
+        }
+      }
+    });
+
+    // Remove duplicates of all the CSS: sass key value pairs
+    const uniqueCssSass = [...new Set(cssSassVarStr.split(','))].join(',').replace(/,/g, ';');
+    
+    generatedScss += `:where(html) { ${uniqueCssSass} }\n`;
+    generatedScss += `@media #{_mq.$OSdark} { :where(html) { ${darkMediaStr} } }`;
+  
+  /=========================
+  // All other Open Props
+  //=========================
   } else {
     Object.entries(importObj).forEach(([key, value]) => {
       if (key.includes('@')) {
-        return;
+        return; // Skip the key-value pair for anything containing @
       }
       key = key.replace('--', '$');
       generatedScss += `${key}: ${value};\n`;
@@ -85,6 +163,8 @@ let indexScss = '';
 for (const moduleName in openPropFiles) {
   indexScss += `@forward '${moduleName}';\n`;
 }
+
+indexScss += `@forward 'config/config';\n`;
 
 const indexOutFile = path.join(__dirname, 'index.scss');
 await fs.writeFile(indexOutFile, indexScss, { encoding: 'utf-8' });
