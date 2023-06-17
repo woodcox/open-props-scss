f06b5e1e30280cd32957d5ad210e7e056b97bb91
Rustie <64870518+woodcox@users.noreply.github.com>
2023-06-17 16:41:13 +0100

    Update generateScss.mjs

--- generateScss.mjs
+++ generateScss.mjs
@@ -107,7 +107,7 @@ const generateSCSSModule = async (moduleName, importObj) => {
     
     Object.entries(importObj).forEach(([key, value]) => {
       key = key.replace('--', '$hd-'); // prevent naming conflict with the grays in colors module
-      value = value.replace(/var\(--(.*?)(?:,\s*(.*?))?\)/g, '#{$$$1} !default');
+      value = value.replace(/var\(--(.*?)(?:,\s*(.*?))?\)/g, '#{$$$1}');
       
       generatedScss += `${key}: ${value};\n`;
     });
