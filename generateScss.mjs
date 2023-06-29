3396d6193fe8039cb7552e3b345ada421b64d835
Rustie <64870518+woodcox@users.noreply.github.com>
2023-06-29 12:55:47 +0100

    Update generateScss.mjs

--- generateScss.mjs
+++ generateScss.mjs
@@ -101,7 +101,7 @@ const generateSCSSModule = async (moduleName, importObj) => {
     Object.entries(oklchcolors).forEach(([key, value]) => {
       key = key.replace('--', '$');
 
-      Object.entries(hueStr).forEach(([colorName, hue]) => {
+      Object.keys(hueStr).forEach((colorName) => {
         if (key.includes('color-')) {
          console.log(colorName);
          oklchName = key.replace('color-', colorName);
