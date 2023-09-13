2f98bb2439bf523408154ea96d59ae1001613d29
Rustie <64870518+woodcox@users.noreply.github.com>
2023-09-13 13:35:05 +0100

    Update generateScss.mjs

--- generateScss.mjs
+++ generateScss.mjs
@@ -95,8 +95,8 @@ const generateSCSSModule = async (moduleName, importObj) => {
       keyframesContent = value.replace(/@keyframes\s+(\S+)/, '@keyframes #{$id}');
     } else if (!key.includes('-@')) {
     //if (typeof value === 'string' && !key.includes('-@')) {
-     // key = key.replace('--', '$');
-      durationAndEasing = value.replace(/(\w+)\s+(\S+)/, '$1-#{$animation-id} $2');
+      key = key.replace('--', '$');
+      durationAndEasing = value.replace(/(\w+)\s+(\S+)/, '$2');
      // const sassVar = value.replace(/var\(--(.*?)\)/g, '#{_e.$$$1}'); // Replace var(--cssvar) with e.$cssvar when they occurs in a value
      // animationsStr += `${key}: ${sassVar} !default;\n`
     }
